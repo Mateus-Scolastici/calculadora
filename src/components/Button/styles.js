1c2cd8b76edd8aa2ca9e9362f0927f7439a448c2
Mateus Scolastici <mateusfontascp2704@gmail.com>
2023-04-07 17:21:40 -0300

    Att
    Arquivos js e package.jason

---
+++ src/components/Button/styles.js
@@ -0,0 +1,15 @@
+import styled from 'styled-components';
+
+export const ButtonContainer = styled.button`
+    padding: 20px;
+    border: 1px solid #000000;
+    background-color: #000FF0;
+    color: #FFFFFF;
+    font-size: 24px;
+    font-weight: 700;
+    flex: 1;
+
+    &: hover{
+        opacity: 0.6;
+    }
+`
---
+++ src/components/Input/styles.js
@@ -0,0 +1,29 @@
+import styled from 'styled-components';
+
+export const InputContainer = styled.div`
+    width: 100%;
+    height: 75px;
+    background-color: #000000;
+
+    display: flex;
+    align-item: center;
+    justify-content: flex-end;
+
+    font-size: 24px;
+    font-family: 'Roboto';
+    
+
+    input{
+        width: 100%;
+        height: 75px;
+        background-color: #000000;
+        border: 0;
+
+        text-align: right;
+        
+        padding: 0 10px;
+        font-size: 24px;
+        font-family: 'Roboto';
+        color: #FFFFFF;
+    }
+`
